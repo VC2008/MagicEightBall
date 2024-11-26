f69818eb72f58eea5bd8767d6ef5c713859ab8c6
VC2008 <526Vcolon@frhsd.com>
2024-11-26 11:51:48 -0500

    Un-IMGS/'d all my images'

--- script.js
+++ script.js
@@ -21,39 +21,39 @@ function shakeMagic8Ball(){
     switch (randomNumber) {
       case 0:
           eightBall = 'Yes';
-          imageChoice = 'IMGS/FrontFacingDjungelskog.png'
+          imageChoice = 'FrontFacingDjungelskog.png'
         break;
       case 1:  
         eightBall = 'It is certain';
-        imageChoice = 'IMGS/FrontFacingDjungelskog.png'
+        imageChoice = 'FrontFacingDjungelskog.png'
         break;
       case 2:
           eightBall = 'It is decidedly so';
-          imageChoice = 'IMGS/FrontFacingDjungelskog.png'
+          imageChoice = 'FrontFacingDjungelskog.png'
         break;
       case 3:
           eightBall = 'Reply hazy try again';
-          imageChoice = 'IMGS/FrontFacingDjungelskog.png'
+          imageChoice = 'FrontFacingDjungelskog.png'
         break;
       case 4:
           eightBall = 'Cannot predict now';
-          imageChoice = 'IMGS/FrontFacingDjungelskog.png'
+          imageChoice = 'FrontFacingDjungelskog.png'
         break;
       case 5:
           eightBall = 'Do not count on it';
-          imageChoice = 'IMGS/FrontFacingDjungelskog.png'
+          imageChoice = 'FrontFacingDjungelskog.png'
         break;
       case 6:
           eightBall = 'No';
-          imageChoice = 'IMGS/FrontFacingDjungelskog.png'
+          imageChoice = 'FrontFacingDjungelskog.png'
         break;
       case 7:
           eightBall = 'It is Beyond My Understanding';
-          imageChoice = 'IMGS/FrontFacingDjungelskog.png'
+          imageChoice = 'FrontFacingDjungelskog.png'
         break;
       case 8:
         eightBall = 'It Seems To Be So';
-        imageChoice = 'IMGS/FrontFacingDjungelskog.png'
+        imageChoice = 'FrontFacingDjungelskog.png'
         break;
         
     }
